7e667dc769be64f36dea58fb105c4e0a09af606b
Lubeth <lubethjimenez@yahoo.com>
2026-05-13 21:54:51 -0400

    Add start up, Database, & verified server is running

---
+++ src/App.jsx
@@ -0,0 +1,36 @@
+
+import { useEffect, useState } from 'react';
+import './App.css'
+import { supabase } from './utils/supabase'
+import StudentCard from './components/StudentCard';
+
+function App() {
+//READ from Database(table)//
+   const [students, setStudents] = useState([]);
+  
+async function fetchStudents() {
+  try {
+    const {data, error} = await supabase.from("Directory").select("*");
+    if(error){
+      console.log(error);
+    } else {
+      console.log(data);
+      setStudents(data);
+    }
+  } catch (error) {
+    console.log(error)
+  } 
+}
+    useEffect(()=>{
+      fetchStudents();
+    }, [])
+
+  return (
+    <div>
+      <h1>React CRUD W6D3</h1>
+      <StudentCard students={students} />
+    </div>
+  )
+}
+
+export default App
